1f939d4230f66d9e62b6aab4b1cbcb75c11b3f61
Abhishek Ranjan <ranjanabhi00@gmail.com>
2024-02-10 16:40:25 +0530

    added features

---
+++ react/src/todo/components/item.jsx
@@ -0,0 +1,82 @@
+import { memo, useState, useCallback, useEffect } from "react";
+import classnames from "classnames";
+import "../app.css"
+
+import { Input } from "./input";
+
+import { TOGGLE_ITEM, REMOVE_ITEM, UPDATE_ITEM, SET_NEWLY_ADDED } from "../constants";
+import { useLocation } from "react-router-dom";
+
+export const Item = memo(function Item({ todo, dispatch, index ,size}) {
+    const [isWritable, setIsWritable] = useState(false);
+    const { title, completed, id ,isNewlyAdded ,createdAt,updatedAt} = todo;
+
+    const { pathname: route } = useLocation();
+
+    const toggleItem = useCallback(() => dispatch({ type: TOGGLE_ITEM, payload: { id } }), [dispatch]);
+    const removeItem = useCallback(() => dispatch({ type: REMOVE_ITEM, payload: { id } }), [dispatch]);
+    const updateItem = useCallback((id, title) => dispatch({ type: UPDATE_ITEM, payload: { id, title } }), [dispatch]);
+    const updateItemColor = useCallback((id) => dispatch({ type: SET_NEWLY_ADDED, payload: { id} }), [dispatch]);
+
+    const handleDoubleClick = useCallback(() => {
+        setIsWritable(true);
+    }, []);
+
+    const handleBlur = useCallback(() => {
+        setIsWritable(false);
+    }, []);
+
+    const handleUpdate = useCallback(
+        (title) => {
+            if (title.length === 0)
+                removeItem(id);
+            else
+                updateItem(id, title);
+
+            setIsWritable(false);
+        },
+        [id, removeItem, updateItem]
+    );
+    useEffect(()=>{
+       let timer= setTimeout(()=>{
+            updateItemColor(id)
+        },5000)
+
+        return ()=>clearTimeout(timer)
+    },[])
+
+    let labelClass=classnames({
+        newly_added:isNewlyAdded,
+        currently_added:!isNewlyAdded
+    })
+
+    if(route=='/completed'){
+        labelClass=classnames({
+            first:index==size-1,
+            second:index==size-2,
+            third:index==size-3
+        })
+    }
+  
+     
+
+    return (
+        <li className={classnames({completed:todo.completed,})} data-testid="todo-item">
+            <div className="view">
+                {isWritable ? (
+                    <Input onSubmit={handleUpdate} label="Edit Todo Input" defaultValue={title} onBlur={handleBlur} />
+                ) : (
+                    <>
+                        <input className="toggle" type="checkbox" data-testid="todo-item-toggle" checked={completed} onChange={toggleItem} />
+                        <label data-testid="todo-item-label" onDoubleClick={handleDoubleClick} className={labelClass} >
+                            {title}
+                        </label>
+                        <span>{createdAt.toUTCString()}</span>
+                        {completed && <span>{updatedAt.toUTCString()}</span>}
+                        <button className="destroy" data-testid="todo-item-button" onClick={removeItem} />
+                    </>
+                )}
+            </div>
+        </li>
+    );
+});
---
+++ react/src/todo/constants.js
@@ -0,0 +1,8 @@
+export const ADD_ITEM = "ADD_ITEM";
+export const UPDATE_ITEM = "UPDATE_ITEM";
+export const REMOVE_ITEM = "REMOVE_ITEM";
+export const TOGGLE_ITEM = "TOGGLE_ITEM";
+export const REMOVE_ALL_ITEMS = "REMOVE_ALL_ITEMS";
+export const TOGGLE_ALL = "TOGGLE_ALL";
+export const REMOVE_COMPLETED_ITEMS = "REMOVE_COMPLETED_ITEMS";
+export const SET_NEWLY_ADDED="SET_NEWLY_ADDED"
